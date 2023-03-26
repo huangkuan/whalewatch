4089cbeff5c9130639d769194e2338517206c34e
Kuan Huang <huangkuan@gmail.com>
2023-03-26 17:19:07 -0400

    initial commit

---
+++ datastore.js
@@ -0,0 +1,12 @@
+import {uniswap_pools} from "./constants.js"
+
+
+function AddUniswapPools2CSV(){
+    for (let up of uniswap_pools){
+        console.log("0" + up['pool'].slice(1) + ',' + 'Uniswap Pool,')
+    }
+    return 1
+}
+
+
+array2CSV()
---
+++ index.js
@@ -0,0 +1,52 @@
+import got from 'got'
+import * as dotenv from 'dotenv'
+import * as fs from 'fs'
+
+dotenv.config()
+const conf = process.env
+const walletAddress = '0xe11970f2f3de9d637fb786f2d869f8fea44195ac'
+const url_es = `${conf.ES_API_MAIN}${conf.API_ERCTRANSFER}&address=${walletAddress}&page=1&offset=100&${conf.ES_BLOCKRANGE}&sort=asc&apikey=${conf.ES_API_KEY}`
+const url_at = `${conf.AT_API_MAIN}${conf.API_ERCTRANSFER}&address=${walletAddress}&page=1&offset=100&${conf.AT_BLOCKRANGE}&sort=asc&apikey=${conf.AT_API_KEY}`
+
+loadDB()
+const addressMap = new Map()
+addressArr.forEach((obj) => {
+    let row = obj.split(',')
+    addressMap.set(row[0], row[1])
+})
+
+console.log("parsing csv done")
+
+const dataES = await got.post(url_es).json()
+const dataAT = await got.post(url_at).json()
+console.log("Ethereum")
+parseAPIResponse(dataES)
+console.log("Arbitrum")
+parseAPIResponse(dataAT)
+
+
+function loadDB(){
+    const content = fs.readFileSync('cexlabels.csv', 'utf8')
+    const addressArr = content.split('\n')
+    const addressMap = new Map()
+    addressArr.forEach((obj) => {
+        let row = obj.split(',')
+        addressMap.set(row[0], row[1])
+    })
+    return addressMap
+}
+
+function parseAPIResponse(data){
+    
+    for (let a of data['result']){
+        const from          = addressMap.get(a['from'])?addressMap.get(a['from']):a['from']
+        const to            = addressMap.get(a['to'])?addressMap.get(a['to']):a['to']
+        const tokenDecimal  = parseInt(a['tokenDecimal'])
+        const tokenValue    = parseInt(a['value'])/Math.pow(10, tokenDecimal)
+        console.log(` from ${from} to ${to} for ${tokenValue} ${a['tokenSymbol']} at ${a['timeStamp']}`)
+    }
+}
+
+function getBlockByTime(){
+
+}
