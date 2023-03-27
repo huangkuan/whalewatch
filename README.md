9652ff16b2d59390a44cc1795fd25c2498a4d3a4
Kuan Huang <huangkuan@gmail.com>
2023-03-27 14:21:29 -0400

    updates on march 27th

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # whalewatch
-watching the on-chain whales
+
+
+##Prototype plan:##
+To have a simple UI that displays what the smart wallets are buying or selling right now. 
+1. 1)As a start, we will collect smart wallet addresses from varioius resources such as known twitter accounts, Nansen and Etherscan.
+   2)In the forseeable future, we can monitor the swaps of Dexes to automatically collect addresses.
+2. We need a way to tell the PnL of a specific wallet
+3. Based on 2, we then can tell what tokens the smart wallet is best at trading.
+4.  1) Based on 3, we then can monitor the trading behavior of a cluster of smart wallets.
+    2) In the forseeable future, we can also include large amount of token buy or sell even if the wallets are not labeled as "smart".
+5. If a user sees a large enough cluster of smart wallets buy or sell a token, the user can follow the same trade.
+
+
+
+Rate limit of Etherscan API: 5 calls per second, 100k calls per day
+#index.js file is the starting point
+Include below in an .env file
+
+ES_API_KEY  = D34PXUZ7YRGSW1GFPQNCUF1SA7VU9TUQ1Q
+ES_API_MAIN = https://api.etherscan.io/api?
+AT_API_KEY          = E65X7P422HUZ7J2CRMYCTUPMZ7P6JYJHUW
+AT_API_MAIN         = https://api.arbiscan.io/api?
+ES_BLOCKRANGE       = 'startblock=16887475'
+AT_BLOCKRANGE       = 'startblock=72825000'
+API_ERCTRANSFER     = 'module=account&action=tokentx'
+
+
 #nothing yet
--- index.js
+++ index.js
@@ -5,25 +5,19 @@ import * as fs from 'fs'
 dotenv.config()
 const conf = process.env
 const walletAddress = '0xe11970f2f3de9d637fb786f2d869f8fea44195ac'
-const url_es = `${conf.ES_API_MAIN}${conf.API_ERCTRANSFER}&address=${walletAddress}&page=1&offset=100&${conf.ES_BLOCKRANGE}&sort=asc&apikey=${conf.ES_API_KEY}`
-const url_at = `${conf.AT_API_MAIN}${conf.API_ERCTRANSFER}&address=${walletAddress}&page=1&offset=100&${conf.AT_BLOCKRANGE}&sort=asc&apikey=${conf.AT_API_KEY}`
+const url_es = `${conf.ES_API_MAIN}${conf.API_ERCTRANSFER}&address=${walletAddress}&page=3&offset=100&${conf.ES_BLOCKRANGE}&sort=asc&apikey=${conf.ES_API_KEY}`
+const url_at = `${conf.AT_API_MAIN}${conf.API_ERCTRANSFER}&address=${walletAddress}&page=1&${conf.AT_BLOCKRANGE}&sort=asc&apikey=${conf.AT_API_KEY}`
 
-loadDB()
-const addressMap = new Map()
-addressArr.forEach((obj) => {
-    let row = obj.split(',')
-    addressMap.set(row[0], row[1])
-})
-
+var addressLabelsMap = loadDB()
 console.log("parsing csv done")
-
 const dataES = await got.post(url_es).json()
 const dataAT = await got.post(url_at).json()
 console.log("Ethereum")
-parseAPIResponse(dataES)
+parseAPIResponse(dataES, addressLabelsMap)
+/*
 console.log("Arbitrum")
 parseAPIResponse(dataAT)
-
+*/
 
 function loadDB(){
     const content = fs.readFileSync('cexlabels.csv', 'utf8')
@@ -36,17 +30,26 @@ function loadDB(){
     return addressMap
 }
 
-function parseAPIResponse(data){
-    
+function parseAPIResponse(data, addressMap){
+    if (data['result'].length == 0)
+        console.log('reading the end')
+    console.log(data['result'].length)
+
+    /*
     for (let a of data['result']){
         const from          = addressMap.get(a['from'])?addressMap.get(a['from']):a['from']
         const to            = addressMap.get(a['to'])?addressMap.get(a['to']):a['to']
         const tokenDecimal  = parseInt(a['tokenDecimal'])
         const tokenValue    = parseInt(a['value'])/Math.pow(10, tokenDecimal)
         console.log(` from ${from} to ${to} for ${tokenValue} ${a['tokenSymbol']} at ${a['timeStamp']}`)
-    }
+    }*/
 }
 
 function getBlockByTime(){
 
+}
+
+
+function retrieveAndParse(){
+    
 }
